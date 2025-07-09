e5533f4e7d6059a84c5953afeea39b8c0f42c1c1
hanzalaareeb <hanzalaareb@gmail.com>
2025-07-09 13:08:43 +0000

    split data set

--- README.md
+++ README.md
@@ -4,4 +4,14 @@
 
 ## Data set I am using :-
 
-Link to data set: Link [[https://www.kaggle.com/datasets/yt776styjsu/labeled-test-set-do-not-submit]]
+- Link to data set: Link [[https://www.kaggle.com/datasets/yt776styjsu/labeled-test-set-do-not-submit]]
+
+### Data spliting rations
+
+1. I manually splitted the dataset into training(70%), validation(15%), and test(15%) sets to ensure fair model evaluation.
+
+2. Make sure your dataset is downloaded:
+
+    ```bash
+        python scripts/download_data.py
+    ```
--- scripts/split_data.py
+++ scripts/split_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("uv/data/data.csv")
-# print(df.head())
+print(df.head())
 
 # shuffling the data set
 df = df.sample(frac=1, random_state=42).reset_index(drop=True) # rn done on every run
@@ -24,3 +25,25 @@ df_test = df.iloc[val_end:]
 
 print(len(df_train), len(df_val), len(df_test))
 
+# target column label
+X_train = df_train.drop(columns=['Label']).values
+y_train = df_train['Label'].values
+
+X_val = df_val.drop(columns=['Label']).values
+y_val = df_val['Label'].values
+
+X_test = df_test.drop(columns=['Label']).values
+y_test = df_test['Label'].values
+
+# <Visualisation> why not
+plt.hist(y_train, bins=10, alpha=0.7, label='train')
+plt.hist(y_val, bins=10, alpha=0.7, label='val')
+plt.hist(y_test, bins=10, alpha=0.7, label='test')
+plt.legend()
+plt.title("Label distribution across splits")
+plt.show()
+
+# Saving in sepreat files
+df_train.to_csv("uv/data/train.csv", index=False)
+df_val.to_csv("uv/data/val.csv", index=False)
+df_test.to_csv("uv/data/test.csv", index=False)
